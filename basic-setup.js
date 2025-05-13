444cfe522165b55d29ceff0434a9ad4b5ab9723d
varunriyer <vs7835@srmist.edu.in>
2025-05-13 12:16:24 +0530

    add basic Express server setup in basic-setup.js

---
+++ basic-setup.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express()
+const PORT = 3000
+
+app.listen(PORT, (error) => {
+    if (!error)
+        console.log(`Server is successfully running and App is listening on port:${PORT}`);
+    else
+        console.log("Error occured, Server can't start", error);
+}
+);
+
